de96f587dc5807322ea94db4090bbd0522c2ab95
benjermanloo1 <benjamin.s.lee13@gmail.com>
2026-05-27 18:34:22 -0400

    added prediction data

--- scrape.py
+++ scrape.py
@@ -10,3 +10,7 @@ team_stats.to_csv("data/raw/team_stats.csv")
 team_schedules = nfl.load_schedules(YEARS)
 team_schedules = team_schedules.to_pandas()
 team_schedules.to_csv("data/raw/team_schedules.csv")
+
+future_games = nfl.load_schedules(2026)
+future_games = future_games.to_pandas()
+future_games.to_csv("data/raw/future_games.csv")
