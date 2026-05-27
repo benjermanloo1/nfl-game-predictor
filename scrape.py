2776037fea5f444889af2688de418844ae2cc730
benjermanloo1 <benjamin.s.lee13@gmail.com>
2026-05-27 18:23:36 -0400

    Combined data sets

--- scrape.py
+++ scrape.py
@@ -1,10 +1,12 @@
 import nflreadpy as nfl
-import pandas as pd
+import pandas
 
 YEARS = list(range(2020, 2026))
 
 team_stats = nfl.load_team_stats(YEARS)
-
 team_stats = team_stats.to_pandas()
-
 team_stats.to_csv("data/raw/team_stats.csv")
+
+team_schedules = nfl.load_schedules(YEARS)
+team_schedules = team_schedules.to_pandas()
+team_schedules.to_csv("data/raw/team_schedules.csv")
